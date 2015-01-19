885dec4758c5298556c4b6ebd726dc9f33abac5d
dbeeke <daniel@studiofonkel.nl>
2015-01-19 21:07:57 +0100

    Built raamwerk from commit 1bd0bf8 on branch master

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+require.config({
+  baseUrl: '/scripts',
+  paths: {
+    jquery: '../bower_components/jquery/dist/jquery',
+    twig: '../bower_components/twig.js/twig',
+    dragdealer: '../bower_components/dragdealer/src/dragdealer',
+    underscore: '../bower_components/underscore/underscore',
+    history: '../bower_components/history/history',
+    objectwatch: '../bower_components/object-watch/index',
+    async: '../bower_components/async/lib/async',
+    marked: '../bower_components/marked/lib/marked'
+  }
+})
+
+require(['jquery', 'raamwerk/context'], function ($, context) {
+
+  'use strict'
+
+  window.transitionEnd = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend'
+  window.animationEnd = 'webkitAnimationEnd oanimationend msAnimationEnd animationend'
+  window.maxWaitForTransition = 3000
+
+  context.init()
+})
